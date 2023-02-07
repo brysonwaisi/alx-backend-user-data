4c0f5da419cb5e10c89a260a79cf08216203afff
brysonwaisi <brysonnyamwange@gmail.com>
2023-02-07 12:40:29 +0300

    Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> None:
     '''GET /api/v1/unauthorized
     Return:
@@ -35,7 +35,7 @@ def unauthorized() -> None:
     abort(401)
 
 
-@app_views.route('/forbidden/', methods=['GET'], strict_flashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_flashes=False)
 def forbidden() -> None:
     ''''GET /api/v1/forbidden
     Return:
