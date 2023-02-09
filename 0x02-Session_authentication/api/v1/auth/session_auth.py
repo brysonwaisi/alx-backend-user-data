3711809c33d2cae941749a3e48a65085b2271650
brysonwaisi <brysonnyamwange@gmail.com>
2023-02-09 21:05:03 +0300

    User ID for session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
-'''SEssion Authentication Module'''
+'''Session Authentication Module'''
 from api.v1.auth.auth import Auth
+from flask import request
 from models.user import User
-import uuid
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     '''Session Auth Class'''
     user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''creates session id for user'''
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        '''Retrieves user id of user associated
+        with session id'''
+        if type(session_id) is str:
+            return self.user_id_by_session_id.get(session_id)
