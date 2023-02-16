bbee53be4f5f5247686545820761fdbc0e9dd5d0
brysonwaisi <brysonnyamwange@gmail.com>
2023-02-16 22:04:09 +0300

    User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,7 +10,7 @@ Base = declarative_base()
 class User(Base):
     '''Represents a record from user table'''
     __tablename__ = "users"
-    id = Column(Interger, primary_key=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
